90560a92649c672a12d9c4118f380c288d86529f
Evan Lee <evan.lee7007@gmail.com>
2022-07-12 00:56:38 +1000

    fixed data_process for scenes with multiple tags

--- human_trajectory_forecasting/five_parallel_synth/data_process.py
+++ human_trajectory_forecasting/five_parallel_synth/data_process.py
@@ -1,6 +1,5 @@
 #import ujson as json
 import pandas as pd
-import re
 
 def grep(tracks, p, s, e):
     primary = []
@@ -18,26 +17,29 @@ def grep(tracks, p, s, e):
     return primary, neighbours
 
 def create_scene(tracks, s_id, p, s, e, tag):
+    scene_list = []
     scene = []
-    scene.extend([s_id])
+    scene.append(s_id)
     primary, neighbours = grep(tracks, p, s, e)
     # {"f": 95, "p": 19, "x": 0.21, "y": 0.45}
-
+    
     for track in sorted(primary, key=lambda x: (x['p'], x['f'])):
         scene.extend([track['x'], track['y']])
 
     for track in sorted(neighbours, key=lambda x: (x['p'], x['f'])):
         scene.extend([track['x'], track['y']])
 
-    scene.append(tag)
+    for i in range(len(tag[-1])):
+        sc_copy = scene
+        sc_copy.append(tag[-1][i])
+        scene_list.append(sc_copy)
 
-    return '\t'.join([str(x) for x in scene])
+    return scene_list
 
 def create_categories():
     categories = ['ID']
     pedestrians = ['P', 'N1', 'N2', 'N3', 'N4']
     for pedestrian in pedestrians:
-        categories.append(pedestrian)
         for i in range(21):
             x = pedestrian + 'X' + str(i)
             y = pedestrian + 'Y' + str(i)
@@ -46,13 +48,17 @@ def create_categories():
 
     return categories
 
-def scene_dat_convert():
+if __name__ == "__main__":
     train_path = "five_parallel_synth/train/orca_five_nontraj_synth.ndjson"
     df = pd.read_json(train_path, lines=True)
 
     # hard coded for efficiency
     df_scenes = df.iloc[0:43697, 0]
     df_tracks = df.iloc[43698: , 1]  
+    
+    categories = create_categories()
+    with open('five_parallel_synth.dat', 'w') as f:
+        f.write('\t'.join([x for x in categories]))
 
     scenes = []
 
@@ -63,14 +69,12 @@ def scene_dat_convert():
         e = df_scene['e']
         tag = df_scene['tag']
 
-        scene = create_scene(df_tracks, s_id, p, s, e, tag)
+        scene_list = create_scene(df_tracks, s_id, p, s, e, tag)
+        for scene in scene_list:
+            scene_str = '\t'.join([str(x) for x in scene])
+            scenes.append(scene_str)
 
-        scenes.append(scene)
-
-    categories = create_categories()
-
-
-    dat_info = ['\t'.join([x for x in categories])] + scenes
-
-    with open('five_parallel_synth.dat', 'w') as f:
-        f.write('\n'.join([line for line in dat_info]))
+    with open('five_parallel_synth.dat', 'a') as f:
+        f.write('\n')
+        for scene in scenes:
+            f.write(f"{scene}\n")
